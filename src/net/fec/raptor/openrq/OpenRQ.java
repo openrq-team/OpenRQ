40f3fb3b1660ab626ee7acd6b65d8899e4d29f6a
ricardof <ricardof@lasige.di.fc.ul.pt>
2014-02-12 16:29:02 +0000

    The beginning...

---
+++ src/net/fec/raptor/openrq/OpenRQ.java
@@ -0,0 +1,29 @@
+package net.fec.raptor.openrq;
+
+/**
+ * @author Jos&#233; Lopes &lt;jlopes&#064;lasige.di.fc.ul.pt&gt;
+ * @author Ricardo Fonseca &lt;ricardof&#064;lasige.di.fc.ul.pt&gt;
+ */
+public final class OpenRQ {
+
+	/**
+	 * @return
+	 */
+	public static RQEncoder newEncoder() {
+
+		return null;
+	}
+
+	/**
+	 * @return
+	 */
+	public static RQDecoder newDecoder() {
+
+		return null;
+	}
+
+	private OpenRQ() {
+
+		// not used
+	}
+}
---
+++ src/net/fec/raptor/openrq/RQEncoder.java
@@ -0,0 +1,12 @@
+package net.fec.raptor.openrq;
+
+
+/**
+ * 
+ * @author Jos&#233; Lopes &lt;jlopes&#064;lasige.di.fc.ul.pt&gt;
+ * @author Ricardo Fonseca &lt;ricardof&#064;lasige.di.fc.ul.pt&gt;
+ */
+public interface RQEncoder {
+
+	
+}
