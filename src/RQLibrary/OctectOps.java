e9b53befa9b2b71d14f080aaaed215d0413773f9
Ricardo Fonseca <ricardof@lasige.di.fc.ul.pt>
2014-03-12 22:46:16 +0000

    Added license text.

--- src/RQLibrary/OctectOps.java
+++ src/RQLibrary/OctectOps.java
@@ -1,3 +1,19 @@
+/* 
+ * Copyright 2014 Jose Lopes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package RQLibrary;
 
 import java.util.Arrays;
@@ -105,7 +121,7 @@ abstract class OctectOps {
 		return betaProduct;
 	}
 	
-	// betas com posiçoes
+	// betas com posicoes
 	protected static final byte[] betaProduct(byte beta, byte[] U, int pos, int length){
 
 //		if(U == null || U.length == 0) throw new IllegalArgumentException("Array must be initialized/allocated.");
