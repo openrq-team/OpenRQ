f8a3e7bca81cda654b5752fa5eb27cec021fbc21
Ricardo Fonseca <ricardof@lasige.di.fc.ul.pt>
2014-03-21 16:35:25 +0000

    Began writing main testing code

--- src/net/fec/openrq/test/TestRunner.java
+++ src/net/fec/openrq/test/TestRunner.java
@@ -16,14 +16,101 @@
 package net.fec.openrq.test;
 
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import net.fec.openrq.core.ArrayDataEncoder;
+import net.fec.openrq.core.encoder.DataEncoder;
+import net.fec.openrq.core.encoder.DataEncoderBuilder;
+import net.fec.openrq.test.encodecode.DecoderTask;
+import net.fec.openrq.test.encodecode.EncoderTask;
+
+
 /**
  * @author Jos&#233; Lopes &lt;jlopes&#064;lasige.di.fc.ul.pt&gt;
  * @author Ricardo Fonseca &lt;ricardof&#064;lasige.di.fc.ul.pt&gt;
  */
 public final class TestRunner {
 
-    public static void main(String[] args) {
+    private static final List<Integer> DATA_SIZES = Collections.unmodifiableList(Arrays.asList(
+        1, 3, 7, 9,
+        10, 13, 17, 99,
+        100, 103, 107, 999,
+        1000, 1003, 1007, 9999,
+        10_000, 10_003, 10_007, 99_999,
+        100_000, 100_003, 100_007, 999_999,
+        1_000_000, 1_000_003, 1_000_007, 9_999_999,
+        10_000_000, 10_000_003, 10_000_007, 99_999_999));
 
-        
+    private static final int WARMUP_SIZE = 1234;
+    private static final long WARMUP_NANOS = TimeUnit.SECONDS.toNanos(15L);
+
+    private static final Random rand = new Random();
+
+
+    private static byte[] makeData(int size) {
+
+        final byte[] data = new byte[size];
+        rand.nextBytes(data);
+        return data;
+    }
+
+
+    private static final class EncProvider implements EncoderTask.DataEncoderProvider {
+
+        private final DataEncoderBuilder<ArrayDataEncoder> builder;
+
+
+        EncProvider(DataEncoderBuilder<ArrayDataEncoder> builder) {
+
+            this.builder = builder;
+        }
+
+        @Override
+        public DataEncoder newEncoder() {
+
+            return builder.build();
+        }
+    }
+
+    private static final class DecChecker implements DecoderTask.DecodedDataChecker {
+
+        private final byte[] originalData;
+
+
+        DecChecker(byte[] originalData) {
+
+            this.originalData = originalData;
+        }
+
+        @Override
+        public boolean checkData(byte[] data) {
+
+            return Arrays.equals(data, originalData);
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        runWarmupTasks(executor, makeData(WARMUP_SIZE));
+    }
+
+    // ===== WARM-UP ===== //
+
+    private static void runWarmupTasks(ExecutorService executor, byte[] data) {
+
+        final long startNanos = System.nanoTime();
+        do {
+
+        }
+        while (System.nanoTime() - startNanos < WARMUP_NANOS);
     }
 }
