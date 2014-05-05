fc197333937adb756504a043f1b6bbc95db69cad
Ricardo Fonseca <ricardof@lasige.di.fc.ul.pt>
2014-05-05 17:45:38 +0100

    Added new test class

---
+++ src/test/benchmark/net/fec/openrq/ArrayAllocationTest.java
@@ -0,0 +1,67 @@
+/* 
+ * Copyright 2014 Jose Lopes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.fec.openrq;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 5)
+@Measurement(iterations = 10)
+@Fork(2)
+@BenchmarkMode(Mode.AverageTime)
+@State(Scope.Benchmark)
+public class ArrayAllocationTest {
+
+    @Param({"1", "10", "100", "1000", "10000", "100000", "1000000", "10000000", "100000000"})
+    public int size;
+    
+    private byte[] preAllocated;
+    
+    @Setup
+    public void setup() {
+        
+        preAllocated = new byte[size];
+    }
+    
+    @GenerateMicroBenchmark
+    public byte[] testPreAllocated() {
+        
+        for (int i = 0, length = size; i < length; ++i) {
+            preAllocated[i] = 0;
+        }
+        
+        return preAllocated;
+    }
+    
+    @GenerateMicroBenchmark
+    public byte[] testNewlyAllocated() {
+        
+        return new byte[size];
+    }
+}
