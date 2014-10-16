32aea73866223c6ce9df03e9833e31993f4d918a
Ricardo Fonseca <ricardof@lasige.di.fc.ul.pt>
2014-10-16 18:35:38 +0100

    Replaced owner in copyright notice

--- src/main/net/fec/openrq/util/linearalgebra/matrix/source/RandomMatrixSource.java
+++ src/main/net/fec/openrq/util/linearalgebra/matrix/source/RandomMatrixSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Jose Lopes
+ * Copyright 2014 OpenRQ Team
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
